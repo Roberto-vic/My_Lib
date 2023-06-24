2b5fb02722409d96207fd5729e4e6c4587d7c61e
Roberto Vicari <roberto.vicari79@gmail.com>
2023-06-24 16:21:57 +0200

    .

---
+++ admin/index.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html lang="de">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="./assets/img/favicon/favicon-32x32.png" type="image/x-icon">
+    <title>My_Lib Admin</title>
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+
+    <!-- Icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <script src="https://kit.fontawesome.com/96e9023ea2.js" crossorigin="anonymous"></script>
+
+    <!-- Style -->
+    <link rel="stylesheet" href="../assets/css/styleAdmin.css">
+
+</head>
+
+<body>
+
+    <!-- Top nav -->
+    <div class="container-fluid">
+        <nav class="navbar fixed-top">
+            <a class="navbar-brand ms-4" href="../index.php"><i class="fa-solid fa-screwdriver-wrench" style="color: #e56815;"> My_Lib Dashboard</i></a>
+            <form class="d-flex ms-auto w-50" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline" type="submit">Suchen</button>
+            </form>
+            <div class="btn-group dropstart ms-auto">
+                <button type="button" class="btn dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
+                    <span class="visually-hidden">Toggle Dropstart</span>
+                </button>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Logout</a></li>
+                </ul>
+                <button type="button" class="btn">
+                    Hallo, Admin
+                </button>
+            </div>
+        </nav>
+    </div>
+
+    <!-- /Top nav -->
+
+    <!-- Sidebar -->
+
+    
+
+    <!-- /Sidebar -->
+
+
+    <!-- Bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+</body>
+
+</html>
